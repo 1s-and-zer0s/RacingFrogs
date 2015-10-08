bc3fa5e15e3b067e6d7f6634378ac82eed7a670c
John Burlile <john.burlile@gmail.com>
2015-10-07 19:49:40 -0600

    Updated lots of stuff

--- app.js
+++ app.js
@@ -1,15 +1,72 @@
 var app = angular.module('racingFrogs', []);
 app.controller('MainController', MainController);
+app.service('RacingService', RacingService);
 //No need to change anything above this line.
+
+function RacingService(){
+    
+}
 
 function MainController() {
     var vm = this; //instead of using this when refering to the controller, let's use vm. It will make things easier.
     
-    vm.joe = new guyConstructor("Joe", 100);
-    vm.bob = new guyConstructor("Bob", 150);
+    vm.joe = new GuyConstructor("Joe", 100);
+    vm.bob = new GuyConstructor("Bob", 150);
     vm.bank = 200;
+    
+    vm.frogList = [];
+    
+    function FrogConstructor(laneNum, name, posX){  
+        this.laneNum = laneNum;
+        this.name = name;
+        this.posX = posX;
+    }
+    
+     vm.addFrogToList = function (frog){
+        vm.frogList.push(frog);
+    }
+    
+    vm.race = function race(){
+        vm.frogList.forEach(function(frog){
+            frog.posX += Math.random();
+        })
+    }
+    
+    function moveFrogs(){
+        for (var i = 0; i < vm.race.length; i++){
+            var frog = vm.race[i];
+            frog.posX += Math.floor(Math.random() * 10);
+        }
+    }
+    
+    function checkWinners(){
+        vm.winners = [];
+        vm.race.forEach(function(frog){
+            if(frog.posX >= finishLine){
+                vm.winners.push(frog);
+            }
+        })
+        if(vm.winners.length > 0){
+            vm.racing = false;
+            
+        }
+    }
+    
+    function testing (){
+        var frog1 = new FrogConstructor(1, "Bill", 1);
+        var frog2 = new FrogConstructor(2,"Frank", 1);
+        var frog3 = new FrogConstructor(3, "Hermit", 1);
+        vm.addFrogToList(frog1);
+        vm.addFrogToList(frog2);
+        vm.addFrogToList(frog3);
+    }
+    testing();
+    
+    
+    
+    
 
-    function guyConstructor(name, startingCash) {
+    function GuyConstructor(name, startingCash) {
 
         this.name = name;
         this.cash = startingCash;
@@ -53,5 +110,4 @@ function MainController() {
         }
     }
 }
-
 
